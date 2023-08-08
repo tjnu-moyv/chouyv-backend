12248c7f72b19868db9d94a818616e035045a8db
SurKaa <suralcarywn@qq.com>
2023-08-08 12:41:47 +0800

    Initial Commit

---
+++ src/main/java/cn/chouyv/ChouYvApplication.java
@@ -0,0 +1,13 @@
+package cn.chouyv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChouYvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChouYvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/chouyv/ChouYvApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chouyv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChouYvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
