e81b9dbb5260965069f165f9084f8adf00cfb5af
SurKaa <suralcarywn@qq.com>
2023-08-09 14:47:57 +0800

    controller 的两个实例

---
+++ src/main/java/cn/chouyv/controller/MoneyController.java
@@ -0,0 +1,19 @@
+package cn.chouyv.controller;
+
+import cn.chouyv.service.MoneyService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author SurKaa
+ */
+@RestController
+@RequestMapping("/money")
+public class MoneyController {
+    
+    private final MoneyService moneyService;
+
+    public MoneyController(MoneyService moneyService) {
+        this.moneyService = moneyService;
+    }
+}
