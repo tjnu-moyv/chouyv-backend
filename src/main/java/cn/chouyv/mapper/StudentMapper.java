50fedf99777fa73dccc3b82e2136d7821ba49662
SurKaa <suralcarywn@qq.com>
2023-08-10 15:47:27 +0800

    mapper中添加通过用户名获取学生 并测试

--- src/main/java/cn/chouyv/mapper/StudentMapper.java
+++ src/main/java/cn/chouyv/mapper/StudentMapper.java
@@ -2,15 +2,24 @@ package cn.chouyv.mapper;
 
 import cn.chouyv.domain.Student;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
-* @author SurKaa
-* @description 针对表【student(用户信息表)】的数据库操作Mapper
-* @createDate 2023-08-08 15:42:54
-* @Entity cn.chouyv.domain.Student
-*/
+ * @author SurKaa
+ * @description 针对表【student(用户信息表)】的数据库操作Mapper
+ * @createDate 2023-08-08 15:42:54
+ * @Entity cn.chouyv.domain.Student
+ */
+@Mapper
 public interface StudentMapper extends BaseMapper<Student> {
 
+    /**
+     * 通过用户名获取学生
+     *
+     * @param username 用户名
+     * @return 学生
+     */
+    Student selectOneByUsername(String username);
 }
 
 
