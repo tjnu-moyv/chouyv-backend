e273bc9d351a106ae562a3765a9fd96b694a37af
SurKaa <suralcarywn@qq.com>
2023-08-11 21:15:49 +0800

    获取学生信息的接口定义

--- src/main/java/cn/chouyv/controller/StudentController.java
+++ src/main/java/cn/chouyv/controller/StudentController.java
@@ -4,10 +4,13 @@ import cn.chouyv.common.request.StudentLoginRequest;
 import cn.chouyv.common.request.StudentRegisterRequest;
 import cn.chouyv.common.response.AuthResponse;
 import cn.chouyv.common.response.BaseResponse;
+import cn.chouyv.common.response.StudentInfoResponse;
 import cn.chouyv.service.StudentService;
 import cn.chouyv.utils.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author SurKaa
@@ -39,6 +42,15 @@ public class StudentController {
     ) {
         log.info("Login: {}", loginRequest);
         AuthResponse response = studentService.loginStudent(loginRequest);
+        return Result.success(response);
+    }
+
+    @GetMapping
+    public BaseResponse<StudentInfoResponse> info(
+            HttpServletRequest request
+    ) {
+        log.info("Info: {}", request);
+        StudentInfoResponse response = studentService.infoStudent(request);
         return Result.success(response);
     }
 }
