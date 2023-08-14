734fb3a2ee5f0ea034dbcc52244f1214167ae18f
SurKaa <suralcarywn@qq.com>
2023-08-14 14:56:31 +0800

    给用户开户

--- src/main/java/cn/chouyv/service/MoneyService.java
+++ src/main/java/cn/chouyv/service/MoneyService.java
@@ -4,10 +4,18 @@ import cn.chouyv.domain.Money;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author SurKaa
-* @description 针对表【money(用户钱包表)】的数据库操作Service
-* @createDate 2023-08-08 15:42:54
-*/
+ * @author SurKaa
+ * @description 针对表【money(用户钱包表)】的数据库操作Service
+ * @createDate 2023-08-08 15:42:54
+ */
 public interface MoneyService extends IService<Money> {
+
+    /**
+     * 给用户开户
+     *
+     * @param uid 开户id
+     * @return 开户信息
+     */
+    Money createMoney(long uid);
 
 }
