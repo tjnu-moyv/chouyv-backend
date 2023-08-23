2e211c2ffd42abf4ae5ede3f81e37135b44e97c0
SurKaa <suralcarywn@qq.com>
2023-08-23 15:22:10 +0800

    fix

--- src/main/java/cn/chouyv/exception/ProductCountException.java
+++ src/main/java/cn/chouyv/exception/ProductCountException.java
@@ -7,13 +7,13 @@ package cn.chouyv.exception;
  * @Date: 2023/08/15/14:31
  * @Description:
  */
-public class ProductCountError extends ChouYvException {
-    public ProductCountError(String message, int code, String description) {
+public class ProductCountException extends ChouYvException {
+    public ProductCountException(String message, int code, String description) {
         super(message, code, description);
     }
 
-    public static ProductCountError error(String description) {
-        return new ProductCountError(
+    public static ProductCountException error(String description) {
+        return new ProductCountException(
                 ChouYvError.PRODUCT_COUNT_WRONG.getMessage(),
                 ChouYvError.PRODUCT_COUNT_WRONG.getCode(),
                 description
